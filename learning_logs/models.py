778e583cdf4683d0cfb0b321f203f9edfd3b9668
Obim Okeke <obim_okeke@baylor.edu>
2020-11-20 08:45:09 -0600

    like the powerpoint

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -20,5 +20,5 @@ class Entry(models.Model):
         verbose_name_plural = 'entries'
 
     def __str__(self):
-        # return f"{self.text[:50]}..."
-        return self.text
+        return f"{self.text[:50]}..."
+        # return self.text
