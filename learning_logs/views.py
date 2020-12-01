2af9ef2cf9e72159ebce3208abfe0f061cecdc33
Obim Okeke <obim_okeke@baylor.edu>
2020-11-30 22:39:48 -0600

    done

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 
@@ -11,8 +13,9 @@ def index(request):
     # The home page for Learning Log.
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner = request.user).order_by('date_added')
     #A context is a dictionary in which the keys are names we'll use
     # in the template to access the data, and the values are the data
     # we need to send to the template. In this case, there's one key-value pair,
@@ -25,6 +28,8 @@ def topics(request):
 def topic(request, topic_id):
     # just like we did in MyShell.py
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     #foreign key can be accessed using '_set'
     entries = topic.entry_set.order_by('date_added') # -date_added is descending order
     context = {'topic':topic, 'entries':entries}
@@ -37,12 +42,15 @@ def new_topic(request):
     else:
         form = TopicForm(data = request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit= False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
@@ -60,9 +68,13 @@ def new_entry(request, topic_id):
     context = {'form': form, 'topic': topic}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id = entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
